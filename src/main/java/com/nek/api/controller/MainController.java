c5c51d46a30243b3607730cfb2d9ee969f26684c
Andrés Cárdenas <mandrescardenash@gmail.com>
2025-12-20 15:29:00 -0500

    add MainController

---
+++ src/main/java/com/nek/api/controller/MainController.java
@@ -0,0 +1,37 @@
+package com.nek.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nek.api.dto.UserRequestDTO;
+import com.nek.api.dto.UserResponseDTO;
+import com.nek.api.service.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    private final UserService userService;
+
+    public MainController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
+        UserResponseDTO createdUser = userService.createUser(userRequestDTO);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public String getAllUsers() {
+        return "Hello from MainController";
+    }
+}
