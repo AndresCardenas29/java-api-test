0534322f975f071183e40a41542722ee810875b6
Andrés Cárdenas <mandrescardenash@gmail.com>
2026-01-22 20:04:05 -0500

    Pull desde shell

--- src/main/java/com/nek/api/controller/MainController.java
+++ src/main/java/com/nek/api/controller/MainController.java
@@ -32,6 +32,6 @@ public class MainController {
 
     @GetMapping
     public String getAllUsers() {
-        return "Hello from MainController intento 25 para autodeploy! pero con Dockerfile optimizado. Probando Dokploy. Prueba para hacer deploy en QA y hacer PR a PROD";
+        return "Hello from MainController intento 25 para autodeploy! pero con Dockerfile optimizado. Probando Dokploy. Prueba para hacer deploy en QA y hacer PR a PROD. Haciendo pr desde la shell";
     }
 }
